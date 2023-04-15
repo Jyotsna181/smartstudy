0b59727178a821a560e17dfb4bc87e67da5798dd
Jyotsna Rasam <41042516+Jyotsna181@users.noreply.github.com>
2023-04-15 17:09:28 +0530

    Update Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,18 +1,44 @@
-import React from "react";
-import {Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
     <header>
-      <a href="#" class="logo">SmartStudy</a>
-      <div id="menu" class="fas fa-bars"></div>
-      <nav className="navbar"> 
-        <Link to="/" className="nav-link">Home</Link>
-        <Link to="/course" className="nav-link">Course</Link>
-        <Link to="/teacher" className="nav-link">Teachers</Link>
-        <Link to="/price" className="nav-link">Price</Link>
-        <Link to="/review" className="nav-link">Review</Link>
-        <Link to="/contact" className="nav-link">Contact</Link>
+      <a href="#" className="logo">
+        SmartStudy
+      </a>
+      <div
+        id="menu"
+        className={`fas ${showMenu ? "fa-times" : "fa-bars"} ${
+          showMenu ? "active" : ""
+        }`}
+        onClick={toggleMenu}
+      ></div>
+      <nav className={`navbar ${showMenu ? "active" : ""}`}>
+        <Link to="/" className="nav-link">
+          Home
+        </Link>
+        <Link to="/course" className="nav-link">
+          Course
+        </Link>
+        <Link to="/teacher" className="nav-link">
+          Teachers
+        </Link>
+        <Link to="/price" className="nav-link">
+          Price
+        </Link>
+        <Link to="/review" className="nav-link">
+          Review
+        </Link>
+        <Link to="/contact" className="nav-link">
+          Contact
+        </Link>
       </nav>
     </header>
   );
